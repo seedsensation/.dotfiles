77e15f3bea9937d627eaa4c16fb519b3796f6dbe
Mercury <m@rcury.com>
2025-07-31 22:21:09 +0100

    Completed my primary setup - see README.org more

---
+++ polybar/.config/polybar/launch_polybar.sh
@@ -0,0 +1,7 @@
+killall -q polybar
+polybar --reload back & disown
+polybar --reload left & disown
+polybar --reload cal & disown
+polybar --reload windowtitle & disown
+polybar --reload right & disown
+polybar --reload monitor2 & disown
