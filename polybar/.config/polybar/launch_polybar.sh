96d2d4b67f38d88f90e024f7d456f044cb716f90
Mercury <m@rcury.com>
2025-08-17 14:08:34 +0100

    Cleared i3

--- polybar/.config/polybar/launch_polybar.sh
+++
@@ -1,7 +0,0 @@
-killall -q polybar
-polybar --reload back & disown
-polybar --reload left & disown
-polybar --reload cal & disown
-polybar --reload windowtitle & disown
-polybar --reload right & disown
-polybar --reload monitor2 & disown
